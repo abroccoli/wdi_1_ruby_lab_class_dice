96885e4e573dbba979c065999219d00302e8e952
Anthony Broccoli <acbrocco@syr.edu>
2015-01-15 15:12:57 -0500

    wrote dice.rb and dice_spec.rb

--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -1,1 +1,19 @@
 require_relative '../lib/dice'
+
+RSpec.describe Dice do
+  let (:sides) {8}
+  subject(:dice) {Dice.new(sides)}
+
+  describe '#initialize' do
+    it 'creates a new instance of a Dice with a number of sides' do
+      expect(dice).to be_a Dice
+    end
+  end
+
+  describe '#roll' do
+    it 'display the random value of a dice roll' do
+      expect(dice.roll).to be_between(1, sides).inclusive
+    end
+  end
+
+end
